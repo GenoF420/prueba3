02c779267e76c55d302c034df9e345983bc0b0ed
imHinotori <hello@hinotori.moe>
2023-06-27 19:24:19 -0400

    Delete paths

--- intranet/urls.py
+++ intranet/urls.py
@@ -7,10 +7,13 @@ urlpatterns = [
     path('users', views.users, name='intranet_users'),
     # path('users/new', views.user_new, name='intranet_user_new'),
     path('user/<username>', views.user, name='intranet_user'),
+    path('user/<username>/delete', views.user_delete, name='intranet_user_delete'),
     path('services', views.services, name='intranet_services'),
     path('service/new', views.service_new, name='intranet_service_new'),
     path('service/<identifier>', views.service, name='intranet_service'),
+    path('service/<identifier>/delete', views.service_delete, name='intranet_service_delete'),
     path('bookings', views.bookings, name='intranet_bookings'),
     # path('booking/new', views.booking_new, name='intranet_booking_new'),
     path('booking/<identifier>', views.booking, name='intranet_booking'),
+    #path('booking/<identifier>/delete', views.booking_delete, name='intranet_booking_delete')
 ]
